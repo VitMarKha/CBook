e73dd0456af3772b5d5d597fc40534b1d4def4ac
VitMarKha <vitmarkha@icloud.com>
2020-09-13 19:18:49 +0700

    Expanding files into folders, adding the first tasks from the second chapter

---
+++ Глава 2. Типы данных, операции и выражения/2.1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int	main(void)
+{
+	int				x;
+	long int		xx;
+	short int		xxx;
+	unsigned int	xxxx;
+	double			d;
+	long double		dd;
+	float			f;
+	char			c;
+	signed char		cc;
+	unsigned char	ccc;
+
+	printf("int в байтах: %lu\nint в max: %d\nint в min: %d\n", sizeof(x), INT_MAX, INT_MIN);
+	printf("long int в байтах: %lu\nlong int в max: %ld\nlong int в min: %ld\n", sizeof(xx), LONG_MAX, LONG_MIN);
+	printf("short int в байтах: %lu\nshort int в max: %d\nshort int в min: %d\n", sizeof(xxx), SHRT_MAX, SHRT_MIN);
+	printf("unsigned int в байтах: %lu\nunsigned int в max: %d\nunsigned int в min: %d\n", sizeof(xxxx), UINT_MAX, 0);
+	printf("double в байтах: %lu\n", sizeof(d));
+	printf("double в байтах: %lu\n", sizeof(dd));
+	printf("float в байтах: %lu\n", sizeof(f));
+	printf("char в байтах: %lu\nchar в max: %d\nchar в min: %d\n", sizeof(c), CHAR_MAX, CHAR_MIN);
+	printf("signed char в байтах: %lu\nsigned char в max: %d\nsigned char в min: %d\n", sizeof(cc), SCHAR_MAX, SCHAR_MIN);
+	printf("unsigned char в байтах: %lu\nunsigned char в max: %d\nunsigned char в min: %d\n", sizeof(ccc), UCHAR_MAX, 0);
+	return (0);
+}
---
+++ Глава 2. Типы данных, операции и выражения/2.2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int	get_line(char s[], int lim);
+
+int	main(void)
+{
+	int		len;
+	char	line[MAXLINE];
+
+	while ((len = get_line(line, MAXLINE)) > 0)
+	{
+		printf("String: %s\n", line);
+	}
+	return (0);
+}
+
+int	get_line(char s[], int lim)
+{
+	int	i;
+	int	c;
+
+	i = 0;
+	while (i < lim - 1)
+	{
+		if ((c = getchar()) != EOF)
+		{
+			if (c != '\n')
+			{
+				s[i] = c;
+				++i;
+			}
+		}
+		else
+			break ;
+	}
+	if (c == '\n')
+	{
+		s[i] = '\n';
+		++i;
+	}
+	s[i] = '\0';
+	return (i);
+}
